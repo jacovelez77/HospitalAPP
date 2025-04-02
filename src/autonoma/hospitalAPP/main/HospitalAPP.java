17bf09d6861ffd6ff1adfb374648c82187a941ac
Jacobo Velez <vjacobo250@gmail.com>
2025-04-01 19:45:21 -0500

    primer commit

---
+++ src/autonoma/hospitalAPP/main/HospitalAPP.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package autonoma.hospitalAPP.main;
+
+/**
+ *
+ * @author esteb
+ */
+
+public class HospitalAPP {
+       public static void main(String[] args) {
+       
+        Biblioteca biblioteca = new Biblioteca();
+        VentanaPrincipal ventana = new VentanaPrincipal(biblioteca);
+        ventana.setVisible(true);
+
+
+        
+    }
+    
+}
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
